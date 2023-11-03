252d6b8bec0f33b813b0f161de05f71751bf824c
Maximiliano Cóppola <maxicopp@outlook.com>
2023-11-02 21:26:54 -0300

    How to randomize the dice images

--- 03-dice/Dicee-iOS13/ViewController.swift
+++ 03-dice/Dicee-iOS13/ViewController.swift
@@ -5,36 +5,24 @@ class ViewController: UIViewController {
     // IBOutlet allows me to reference a UI element
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
-    
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
-
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = [
+        let diceArray = [
             UIImage(imageLiteralResourceName: "DiceOne"),
             UIImage(imageLiteralResourceName: "DiceTwo"),
             UIImage(imageLiteralResourceName: "DiceThree"),
             UIImage(imageLiteralResourceName: "DiceFour"),
             UIImage(imageLiteralResourceName: "DiceFive"),
             UIImage(imageLiteralResourceName: "DiceSix")
-        ][leftDiceNumber]
-        
-        diceImageView2.image = [
-            UIImage(imageLiteralResourceName: "DiceOne"),
-            UIImage(imageLiteralResourceName: "DiceTwo"),
-            UIImage(imageLiteralResourceName: "DiceThree"),
-            UIImage(imageLiteralResourceName: "DiceFour"),
-            UIImage(imageLiteralResourceName: "DiceFive"),
-            UIImage(imageLiteralResourceName: "DiceSix")
-        ][rightDiceNumber]
-        
-        leftDiceNumber = leftDiceNumber + 1
-        rightDiceNumber = rightDiceNumber - 1
+        ]
+
+        // Another way to do it
+        // diceImageView1.image = diceArray.randomElement()
+        // diceImageView2.image = diceArray.randomElement()
+
+        diceImageView1.image = diceArray[Int.random(in: 0...5)]
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
+
     }
 
 }
