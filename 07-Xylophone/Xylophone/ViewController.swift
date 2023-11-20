37cd3979f51bafbe9b688986e60aa3121295c254
Maximiliano Cóppola <maxicopp@outlook.com>
2023-11-20 13:59:15 -0300

    playSound

--- 07-Xylophone/Xylophone/ViewController.swift
+++ 07-Xylophone/Xylophone/ViewController.swift
@@ -1,7 +1,10 @@
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var audioPlayer: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -9,7 +12,20 @@ class ViewController: UIViewController {
     
 
     @IBAction func keyPressed(_ sender: UIButton) {
-        print("click")
+        playSound()
+    }
+    
+    func playSound() {
+        if let path = Bundle.main.path(forResource: "C.wav", ofType:nil) {
+            let url = URL(fileURLWithPath: path)
+
+            do {
+                audioPlayer = try AVAudioPlayer(contentsOf: url)
+                audioPlayer?.play()
+            } catch {
+                // couldn't load file :(
+            }
+        }
     }
 }
 
