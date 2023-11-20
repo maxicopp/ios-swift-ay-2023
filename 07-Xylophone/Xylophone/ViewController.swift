139b54a4cc6d19daee51b706521bf6b9b234eeaf
Maximiliano Cóppola <maxicopp@outlook.com>
2023-11-20 19:11:19 -0300

    Playing different xylophone sounds

--- 07-Xylophone/Xylophone/ViewController.swift
+++ 07-Xylophone/Xylophone/ViewController.swift
@@ -3,23 +3,30 @@ import UIKit
 import AVFoundation
 
 class ViewController: UIViewController {
-
+    
     var audioPlayer: AVAudioPlayer?
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-
+    
     @IBAction func keyPressed(_ sender: UIButton) {
-        print(sender.currentTitle)
-        playSound()
+        sender.alpha = 0.5
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.alpha = 1.0
+        }
+        
+        if let note = sender.currentTitle {
+            playSound(soundName: note)
+        }
     }
-
-    func playSound() {
-        if let path = Bundle.main.path(forResource: "C.wav", ofType:nil) {
+    
+    func playSound(soundName: String) {
+        if let path = Bundle.main.path(forResource: soundName, ofType:"wav") {
             let url = URL(fileURLWithPath: path)
-
+            
             do {
                 audioPlayer = try AVAudioPlayer(contentsOf: url)
                 audioPlayer?.play()
