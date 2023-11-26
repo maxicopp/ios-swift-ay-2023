d54234fe714e53135a6aad70eaffc86aaa202c34
Maximiliano Cóppola <maxicopp@outlook.com>
2023-11-25 21:44:02 -0300

    Calculating the progress bar percentage

--- 08-EggTimer/EggTimer/ViewController.swift
+++ 08-EggTimer/EggTimer/ViewController.swift
@@ -12,22 +12,31 @@ class ViewController: UIViewController {
         let hardness = sender.currentTitle!
         
         if let time = eggTimes[hardness] {
-            startCountdown(time: time)
+            startCountdown(time: Double(time))
+            progressBar.progress = 0.0
         } else {
             print("Hardness not found")
         }
     }
     
-    func startCountdown(time: Int) {
+    
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    func startCountdown(time: Double) {
         var secondsRemaining = time
         
-        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { (Timer) in
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { [weak self] timer in
             if secondsRemaining > 0 {
                 print("\(secondsRemaining) seconds.")
                 secondsRemaining -= 1
+                
+                // Update progress bar
+                let percentageComplete = 1 - (secondsRemaining / time)
+                self?.progressBar.progress = Float(percentageComplete)
+                
             } else {
-                Timer.invalidate()
-                self.titleLabel.text = "DONE!"
+                timer.invalidate()
+                self?.titleLabel.text = "DONE!"
             }
         }
     }
