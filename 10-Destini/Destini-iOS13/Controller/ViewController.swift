e24f5132fe9aebbd93f0e753525fe2dbb5125a49
Maximiliano Cóppola <maxicopp@outlook.com>
2024-01-02 01:33:36 -0300

    iOS App design pattern challenge

---
+++ 10-Destini/Destini-iOS13/Controller/ViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    var storyBrain = StoryBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI()
+    }
+    
+    func updateUI() {
+        storyLabel.text = storyBrain.getStoryTitle()
+        choice1Button.setTitle(storyBrain.getChoice1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getChoice2(), for: .normal)
+    }
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        let userChoice = sender.currentTitle!
+        storyBrain.nextStory(userChoice: userChoice)
+        updateUI()
+    }
+}
